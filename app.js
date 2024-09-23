a5aeeed9a2a6e796d79c69e7ee1988f60f1a3afa
srikeerthireddy <srikeerthireddy24@gmail.com>
2024-09-23 14:10:43 +0530

    Instagram-clone

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.get("/test", authorizeUser, (req, res) => {
   res.status(200).json({ success: true, user: req.user });
 });
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 sequelize
